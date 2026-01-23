b07dbfa94ec1058d8b614f6f7761937503f73c5e
Jacob Möller <jacob.moller@ductus.se>
2026-01-23 14:34:30 +0100

    mig service and docs

--- code-with-quarkus/src/main/java/org/acme/api/UserResource.java
+++ code-with-quarkus/src/main/java/org/acme/api/UserResource.java
@@ -3,11 +3,14 @@ package org.acme.api;
 import org.acme.api.dto.UserRequest;
 import org.acme.api.dto.UserResponse;
 import org.acme.service.UserService;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.parameters.Parameter;
 
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
 
 @Path("/users")
@@ -22,8 +25,33 @@ public class UserResource {
     }
 
     @POST
+    @Path("/createJson")
+    @Operation(
+        summary = "post a user to the db using JSON request body",
+        description = "the firs simple endpoint"
+    )
     public UserResponse createUser(UserRequest req) {
         return UserResponse.UserResponsFromEntity(service.create(req));
+    }
+
+    @POST
+    @Path("/createParam")
+    @Operation(
+        summary = "post a user to the db using this param dependent endpoint instead of a JSON request body",
+        description = "can add descriptions as well"
+    )
+    public UserResponse createUserWParam(
+        @Parameter(
+            description = "a unique username",
+            required = true,
+            example = "jacobTest"
+        )
+        @QueryParam("username") String username
+    ) {
+        UserRequest req = new UserRequest();
+        req.username = username;
+        return UserResponse.UserResponsFromEntity(service.create(req));
+        
     }
 
 }
--- code-with-quarkus/src/main/java/org/acme/api/dto/UserRequest.java
+++ code-with-quarkus/src/main/java/org/acme/api/dto/UserRequest.java
@@ -1,5 +1,8 @@
 package org.acme.api.dto;
 
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
 public class UserRequest {
+    @Schema(required = true, examples = {"jacob", "Daniel"}, description = "unique username")
     public String username;
 }
